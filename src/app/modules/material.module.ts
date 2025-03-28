f72efd4cd5d3acbb944b255b136d782c3c877502
Kage Saluone <ksaluone17@gmail.com>
2025-03-28 16:38:00 -0600

    Completed pt1 nested components

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -8,6 +8,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
 
 
 
@@ -23,6 +24,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatFormFieldModule,
     ReactiveFormsModule,
+    MatTabsModule,
   ],
   exports: [
     MatToolbarModule,
@@ -33,6 +35,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatFormFieldModule,
     ReactiveFormsModule,
+    MatTabsModule,
   ]
 })
 export class MaterialModule { }
