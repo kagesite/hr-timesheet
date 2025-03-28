ea7ab0b3a526cf9067e61c0bcd49b57304e1bd16
Kage Saluone <ksaluone17@gmail.com>
2025-03-28 09:59:19 -0600

    Completed routing with params

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DepartmentsService } from '../../services/departments.service';
+import { Department } from '../../interfaces/department';
 
 @Component({
-  selector: 'app-timesheet',
-  standalone: false,
-  templateUrl: './timesheet.component.html',
-  styleUrl: './timesheet.component.scss'
+    selector: 'app-timesheet',
+    standalone: false,
+    templateUrl: './timesheet.component.html',
+    styleUrl: './timesheet.component.scss'
 })
-export class TimesheetComponent {
+export class TimesheetComponent implements OnInit {
+    departments: Department[] | undefined;
+    department: Department | undefined
+    
+    constructor(
+        private router: ActivatedRoute,
+        private departmentsService: DepartmentsService,
+    ) { }
 
+    ngOnInit(): void {
+        this.departments = this.departmentsService.departments;
+        this.department = this.departments.find(department => department.id === this.router.snapshot.params['id']);
+    }
 }
