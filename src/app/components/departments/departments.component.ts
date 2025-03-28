0c14a0dacd0231982762eb2c061fd3396c8014c4
Kage Saluone <ksaluone17@gmail.com>
2025-03-28 09:38:17 -0600

    Completed pt2 Service Injection and Display Departments

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepartmentsService } from '../../services/departments.service';
+import { Department } from '../../interfaces/department';
 
 @Component({
-  selector: 'app-departments',
-  standalone: false,
-  templateUrl: './departments.component.html',
-  styleUrl: './departments.component.scss'
+    selector: 'app-departments',
+    standalone: false,
+    templateUrl: './departments.component.html',
+    styleUrl: './departments.component.scss'
 })
-export class DepartmentsComponent {
+export class DepartmentsComponent implements OnInit {
+    departments: Department[] | undefined;
+
+    constructor(
+        private departmentsService: DepartmentsService,
+    ) { }
+
+    ngOnInit(): void {
+        this.departments = this.departmentsService.departments;
+    }
 
 }
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 
 
@@ -13,11 +15,15 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
+    MatListModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
